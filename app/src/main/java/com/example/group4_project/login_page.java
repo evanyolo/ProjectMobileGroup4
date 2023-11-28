3633fc1d3d56f14b16a90217c36bdabef9cc89d3
Evan Lo <evan.loano@gmail.com>
2023-11-28 22:07:10 +0700

    added icon, inputtext, and also logo in login page, register page

--- app/src/main/java/com/example/group4_project/login_page.java
+++ app/src/main/java/com/example/group4_project/login_page.java
@@ -1,12 +1,32 @@
 package com.example.group4_project;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class login_page extends AppCompatActivity {
+
+    private EditText etEmail, etPass;
+    private String email, pass;
+    private RequestQueue requestQueue;
+    private String url ="http://192.168.1.11/File Mobile/projek/login.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +42,46 @@ public class login_page extends AppCompatActivity {
 
 
 
+    }
+    public void login (View v){
+        email = etEmail.getText().toString().trim();
+        pass = etPass.getText().toString().trim();
+        if (!email.equals("") && !pass.equals("")) {
+            StringRequest sr = new StringRequest(Request.Method.POST, url,
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            if (response.equals("success")) {
+                                Intent i = new Intent(login_page.this, homepage.class);
+                                startActivity(i);
+                                finish();
+                            } else if (response.equals("failed")) {
+                                Toast.makeText(login_page.this, "Invalid login", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            // Handle Volley error here (e.g., display an error message)
+                            Toast.makeText(login_page.this, "Error occurred", Toast.LENGTH_SHORT).show();
+                        }
+                    }){
+                @Nullable
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String> data = new HashMap<>();
+                    data.put("email",email);
+                    data.put("password",pass);
+
+                    return data;
+                }
+            };
+            requestQueue = Volley.newRequestQueue(getApplicationContext());
+            requestQueue.add(sr);
+        }else {
+            Toast.makeText(this, "Fields Can not be empty",Toast.LENGTH_SHORT).show();
+        }
     }
 
     @Override
--- app/src/main/java/com/example/group4_project/signup_page.java
+++ app/src/main/java/com/example/group4_project/signup_page.java
@@ -1,14 +1,96 @@
 package com.example.group4_project;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class signup_page extends AppCompatActivity {
 
+private EditText etUsername, etEmail,etPass, etNumber,etRepassword;
+private RequestQueue requestQueue;
+private TextView tvStat;
+private String url ="http://192.168.1.11/File Mobile/projek/register.php";
+private String username, email, password, number, repass;
+Button btnRegis;
+Button confirm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_page);
+        etUsername = findViewById(R.id.inputUsername);
+        etEmail = findViewById(R.id.inputEmail);
+        etPass = findViewById(R.id.inputPassword);
+        etNumber = findViewById(R.id.inputPhone);
+        btnRegis = findViewById(R.id.btnRegis);
+        tvStat = findViewById(R.id.tvstatus);
+        etRepassword = findViewById(R.id.inputRepass);
+        btnRegis = findViewById(R.id.btnRegis);
+
     }
+    public void save (View v){
+       username = etUsername.getText().toString().trim();
+       email = etEmail.getText().toString().trim();
+       password = etPass.getText().toString().trim();
+       number = etNumber.getText().toString().trim();
+       repass = etRepassword.getText().toString().trim();
+       if(!password.equals(repass)){
+           Toast.makeText(this,"Password didn't match",Toast.LENGTH_SHORT).show();
+       } else if (!username.equals("")&&!email.equals("")&&!password.equals("")) {
+           StringRequest sr = new StringRequest(Request.Method.POST, url,
+                   new Response.Listener<String>() {
+                       @Override
+                       public void onResponse(String response) {
+                           if (response.equals("success")) {
+                                tvStat.setText("Successfully registered");
+                                btnRegis.setClickable(false);
+                           } else if (response.equals("failed")) {
+                               tvStat.setText("Something wrong");
+
+                           }
+                       }
+                   },
+                   new Response.ErrorListener() {
+                       @Override
+                       public void onErrorResponse(VolleyError error) {
+                           // Handle Volley error here (e.g., display an error message)
+                           Toast.makeText(signup_page.this, "Error occurred", Toast.LENGTH_SHORT).show();
+                       }
+                   }){
+               @Nullable
+               @Override
+               protected Map<String, String> getParams() throws AuthFailureError {
+                   Map<String, String> data = new HashMap<>();
+                   data.put("name_customer",username);
+                   data.put("email",email);
+                   data.put("phone",number);
+                   data.put("password",password);
+                   return data;
+               }
+           };
+           requestQueue = Volley.newRequestQueue(getApplicationContext());
+           requestQueue.add(sr);
+       }
+
+
+    }
+
+
 }
