4fd42b479a65c0434490e083e4f62062e6af3d8c
Alpen <nicholasalven02@gmail.com>
2023-12-11 19:41:57 +0700

    front end

--- app/src/main/java/com/example/group4_project/MapsAbout.java
+++ app/src/main/java/com/example/group4_project/MapsAbout.java
@@ -1,18 +1,27 @@
 package com.example.group4_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 
-public class MapsAbout extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+
+public class MapsAbout extends AppCompatActivity implements OnMapReadyCallback{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps_about);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapsGoogle);
+        mapFragment.getMapAsync(this);
+    }
 
-        Bundle bundle = getIntent().getExtras();
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
 
     }
 }
